9c19fb5ef3b13d7044a606faf58532a11de7b3a2
Jerek Dain <jerekdain@gmail.com>
2017-09-03 15:30:17 -0500

    add a method to play the episodes directly on the site

---
+++ public/js/base.js
@@ -0,0 +1,27 @@
+function displayPlayControls(episodePath) {
+    // If we already revealed the play controls don't do it again
+    if (this.getAttribute('disabled') === 'disabled') {
+        return;
+    }
+
+    // Find the area to
+    var $playDiv = $(this).parent().siblings('.play');
+    if (!$playDiv.length) {
+        alert('Failed to create play controls!');
+        this.setAttribute('disabled', 'disabled');
+        return;
+    }
+
+    var audio = document.createElement('audio');
+    audio.innerHTML = 'Your browser does not support the audio element.';
+    audio.controls = 'controls';
+    audio.src = '/downloads/' + episodePath;
+    audio.type = 'audio/mpeg';
+    $playDiv.append(audio);
+
+    // Disable this so we don't do it multiple times
+    this.setAttribute('disabled', 'disabled');
+
+    // Start playing it
+    audio.play();
+}
